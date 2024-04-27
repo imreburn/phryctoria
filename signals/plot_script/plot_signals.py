311475f042a112f0bfda56d81c459f8cd21a7c46
Jinhong Choi <jinhong.choi@gmail.com>
2024-04-26 18:14:14 -0700

    python script for plotting signals

--- signals/plot_script/plot_signals.py
+++ signals/plot_script/plot_signals.py
@@ -18,30 +18,35 @@ for index, filename in enumerate(p):
             t.append(int(row[0]))
             x.append(int(row[1]))
         
-        zeros = []
+        
         for i in range(1, len(x)):
             if x[i] * x[i-1] < 0:
                 loc = ((t[i-1] * x[i]) - (t[i] * x[i-1])) / (x[i] - x[i-1])
-                zeros.append((i, loc))
+                t.append(loc)
+                x.append(0)
 
         print(t)
         print(x)
-        print(zeros)
+        # print(zeros)
 
+        t1 = [t0 for t0, _ in sorted(zip(t, x))]
+        x1 = [x0 for _, x0 in sorted(zip(t, x))]
+        for i in range(1, len(x1)):
+            if x1[i] > 0:
+                x1[i] = 1
+            elif x1[i] < 0:
+                x1[i] = -1
         
-        for cnt, (i, loc) in enumerate(zeros):
-            t.insert(i+cnt, loc)
-            x.insert(i+cnt, 0)
+        print(t1)
+        print(x1)
 
-        print(t)
-        print(x)
         # print(filename.name)
-        axs[index].plot(t, x)
+        axs[index].plot(t1, x1)
         axs[index].set_title(filename.name)
         axs[index].axhline(0, color='black')
         axs[index].get_xaxis().set_visible(False)
         axs[index].get_yaxis().set_visible(False)
-        for t0, x0 in zip(t, x):
+        for t0, x0 in zip(t1, x1):
             if x0 == 0:
                 axs[index].annotate('%s' % t0, xy=(t0, x0), textcoords='data')
 
